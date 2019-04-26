e791a0412016a69485fc4b282efa72fd0e3a93d5
anyango55 <anyangoc55@gmail.com>
2019-04-26 22:05:08 +0300

    "operations and strings intro"

---
+++ INTRO.py
@@ -0,0 +1,54 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 2+3
+5
+>>> 9-8
+1
+>>> 4*6
+24
+>>> 8/4
+2.0
+>>> 5/2
+2.5
+>>> 5//2
+2
+>>> 8 + 9 - 10
+7
+>>> 8 + 2 * 3
+14
+>>> (8 + 2) * 3
+30
+>>> 2**3
+8
+>>> 10%3
+1
+>>> 'navin'
+'navin'
+>>> print('navin')
+navin
+>>> print('navin's laptop')
+	  
+SyntaxError: invalid syntax
+>>> print("navin's laptop")
+	  
+navin's laptop
+>>> print ('navin "laptop"')
+	  
+navin "laptop"
+>>> print('navin's "laptop"')
+	  
+SyntaxError: invalid syntax
+>>> print('navin\'s "laptop"')
+	  
+navin's "laptop"
+>>> 10*'navin'
+	  
+'navinnavinnavinnavinnavinnavinnavinnavinnavinnavin'
+>>> print('c:\docs\navin')
+	  
+c:\docs
+avin
+>>> print(r'c:docs\navin')
+	  
+c:docs\navin
+>>> 
