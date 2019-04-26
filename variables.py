bf6f94300f608f1c0ed0b6d62626d0eda22e0170
anyango55 <43138367+anyango55@users.noreply.github.com>
2019-04-26 22:20:28 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,72 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 2+3
+5
+>>> x=2
+>>> x=2//assignment operator
+SyntaxError: invalid syntax
+>>> x=2
+>>> x+3
+5
+>>> y=3
+>>> x+y
+5
+>>> x+9
+11
+>>> x=9
+>>> x+y
+12
+>>> x
+9
+>>> x+10
+19
+>>> _+y
+22
+>>> name = 'youtube'
+>>> name
+'youtube'
+>>> name + 'rocks'
+'youtuberocks'
+>>> name + ' rocks'
+'youtube rocks'
+>>> name[0]
+'y'
+>>> name[6]
+'e'
+>>> name[8]
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    name[8]
+IndexError: string index out of range
+>>> name[-1]
+'e'
+>>> name[-2]
+'b'
+>>> name[o:2]
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    name[o:2]
+NameError: name 'o' is not defined
+>>> name[0:2]
+'yo'
+>>> name[1:4]
+'out'
+>>> name[1:]
+'outube'
+>>> name[:4]
+'yout'
+>>> name[3:10]
+'tube'
+>>> name[0:3] = 'my'
+Traceback (most recent call last):
+  File "<pyshell#28>", line 1, in <module>
+    name[0:3] = 'my'
+TypeError: 'str' object does not support item assignment
+>>> 'my' + name[3:]
+'mytube'
+>>> myname = ,navin reddy'
+SyntaxError: invalid syntax
+>>> myname = 'navin reddy'
+>>> len(myname)
+11
+>>> 
