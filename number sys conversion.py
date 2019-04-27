1f35a51377b329fef459db4a02b76e9c087e21de
anyango55 <43138367+anyango55@users.noreply.github.com>
2019-04-27 08:56:40 +0300

    Add files via upload

---
+++ number sys conversion.py
@@ -0,0 +1,24 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> bin(25)
+'0b11001'
+>>> 0b0101
+5
+>>> oct(25)
+'0o31'
+>>> 0o31
+25
+>>> hex(25)
+'0x19'
+>>> hex(10)
+'0xa'
+>>> oxf
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    oxf
+NameError: name 'oxf' is not defined
+>>> 0xf
+15
+>>> 0b110011010
+410
+>>> 
