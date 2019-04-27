50b4cb7e8c3857ec6898934932926acfe3cba614
anyango55 <43138367+anyango55@users.noreply.github.com>
2019-04-27 08:42:02 +0300

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,84 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=2
+>>> y=3
+>>> x+y
+5
+>>> x-y
+-1
+>>> x*y
+6
+>>> x/y
+0.6666666666666666
+>>> x=x+x
+>>> x
+4
+>>> x+=2
+>>> x
+6
+>>> x*=3
+>>> x
+18
+>>> a,b=5,6
+>>> a
+5
+>>> b
+6
+>>> //Unary
+SyntaxError: invalid syntax
+>>> 
+>>> n=7
+>>> n
+7
+>>> -n
+-7
+>>> n
+7
+>>> n=-n
+>>> n
+-7
+>>> a<b
+True
+>>> a>b
+False
+>>> a==b
+False
+>>> a=6
+>>> a==b
+True
+>>> a<=b
+True
+>>> a>=b
+True
+>>> a!=b
+False
+>>> b=7
+>>> a!=b
+True
+>>> //logical
+SyntaxError: invalid syntax
+>>> k=5
+>>> l=4
+>>> a<8
+True
+>>> a<8 and b<5
+False
+>>> a<8 and b<2
+False
+>>> a=5
+>>> b=4
+>>> a<8 and b<5
+True
+>>> a<8 and b<2
+False
+>>> a<8 or b>2
+True
+>>> x =True
+>>> x
+True
+>>> not x
+False
+>>> x = not x
+>>> x
+False
+>>> 
