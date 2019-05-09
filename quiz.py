0c77794bd1d0478867f0055bfaf060ff78b802a1
anyango55 <43138367+anyango55@users.noreply.github.com>
2019-05-09 18:52:38 +0300

    Add files via upload
    "Quiz Two"

---
+++ quiz.py
@@ -0,0 +1,54 @@
+def rem(numbers):
+    numbers=[]
+    list=[]
+    for x in numbers
+        list=(x%3)
+        return(list)
+
+def square(square):
+    square=[x*x for x in range(149,159)]
+    print(square)
+
+def smallest(g):
+    g=[]
+    print(min(g))
+
+
+
+def sorted(a):
+    a=[]
+    b=[]
+    c=[]
+    d=a+b+c
+    d.sort()
+    print(d)
+
+
+def divisible_by_three():
+    m=1
+    n=10
+    print((range(m, n+1)))
+
+def flatten(k):
+    k=[]
+    for sublist in x:
+        for item in sublist:
+            k.append(item)
+            print(k)
+
+def duplicates(li):
+    newli=[]
+    seen=set()
+    for item in li:
+        if item not in seen:
+            seen.add(item)
+            newli.append(item)
+        return(newli)
+    
+
+
+
+def dictionary(self,Name,age):
+        self.Name=Name
+        self.age=age 
+    print("Hello, {} your age is {} Ksh".format(Name,age))
